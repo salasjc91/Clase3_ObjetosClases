3cb6f308b215462fdaaaeb7792d7f2be7b21e10a
salasjc <salasjc@HRD6402574>
2024-02-07 02:50:31 -0600

    Add project files.

---
+++ Clase3_ObjetosClases/Empleo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3_ObjetosClases
+{
+     class Empleo
+    {
+        public string Empresa { get; set; }// propiedades
+        public string DescripcionPuesto { get; set; }
+        public double Salario { get; set; }
+        public string Horario { get; set; }
+
+        int IDEmpleo; //atributo privado
+
+
+        //Constructores
+        public Empleo()//dar valor a las propiedades 
+        {
+            IDEmpleo = 0;
+            Empresa = "";
+            DescripcionPuesto = "";
+            Horario = "";
+            Salario = 0;
+        }
+
+        public Empleo (int id, string empresa)
+        {
+            IDEmpleo = id;
+            Empresa = empresa;
+            DescripcionPuesto = "";
+            Horario = "";
+            Salario = 0;
+        }
+
+
+
+        //Metodos
+        public void Postular(string persona)
+        {
+            Console.WriteLine($"Hola {persona}, tu entrevista para {DescripcionPuesto} es el jueves.");
+        }
+    }
+}
---
+++ Clase3_ObjetosClases/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Clase3_ObjetosClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //crea instancia
+            //manera 1:
+            Empleo e1 = new Empleo();
+            e1.DescripcionPuesto = "Administrador de tienda";
+            e1.Salario = 9000;
+            e1.Postular("Cesar");
+            //manera 2:
+            Empleo e2;                     
+            e2 = new Empleo()
+            {
+                Empresa="BSCI",DescripcionPuesto="Auxiliar de bodega", Salario = 450000
+            };
+            e2.Postular("Cesar");
+
+            
+
+             
+
+        }
+    }
+}
